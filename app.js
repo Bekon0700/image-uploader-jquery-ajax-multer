9c4f23766f361059c3188d8b8e18a4bdae384fe0
Emdadul Islam <43648698+Bekon0700@users.noreply.github.com>
2022-12-28 09:17:07 +0600

    final

--- app.js
+++ app.js
@@ -1,32 +1,52 @@
-const express = require('express')
-const cors = require('cors')
-const multer = require('multer')
+// const express = require('express')
+// const cors = require('cors')
+// const multer = require('multer')
+// const bodyParser = require('body-parser')
 
-const app = express()
+// const app = express()
+
+// app.use(cors())
+
+// app.use(bodyParser.urlencoded({ extended: true }))
+
+
+
+// // upload.array('img', 5),
+// app.post('/upload-img', (req, res) => {
+
+//     const storage = multer.diskStorage({
+//         destination: (req, file, cb) => {
+//             // console.log(file)
+//             cb(null, 'images')
+//         },
+//         filename: (req, file, cb) => {
+//             cb(null, file.originalname)
+//         }
+//     })
+
+//     const upload = multer({ storage: storage }).single('img')
+//     upload(req, res, (err) => {
+//         console.log(req.file)
+//     })
+// })
+
+let express = require('express')
+let cors = require('cors')
+let app = express()
+const multer = require('multer')
+const storage = multer.memoryStorage()
+const upload = multer({ dest: "uploads/" })
 
 app.use(cors())
 
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        console.log(file)
-        cb(null, 'images')
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname)
-    }
+app.get('/', function (req, res, next) {
+    res.json({ msg: 'This is CORS-enabled for all origins!' })
 })
 
-const upload = multer({storage: storage})
-
-app.post('/upload-img', upload.single('img'), (req, res) => {
-    console.log(req.file)
-    res.status(200).json({
-        status: 'Upload Successfully'
-    })
+app.post('/upload-image', upload.single('image'), (req, res) => {
+res.json({status: true, msg: "Image Uploaded"})
 })
 
-
-app.listen(4500, () => {
-    console.log('Server is running on port 4500...')
+app.listen(5000, function () {
+    console.log('Server listening on port 5000')
 })
--- client/main.js
+++ client/main.js
@@ -1,35 +1,116 @@
-$('.submit-form').on('submit', function(e){
+// initial image div markup maker
+const imgDivMaker = (id, img) => {
+    const tempPath = URL.createObjectURL(img) // creating the temporary image url for the src attribute in img tag
+
+    $('.img-container').append(`
+        <div id="outer-box-${id}" class="outer-box">
+            <img src="" alt="" id="img-${id}" class="img">
+            <div id="box-overlay-${id}" class="box-overlay asdasd asd"></div>
+            <p id="title-${id}" class="title"></p>
+        </div>
+        `)
+    $(`#img-${id}`).attr('src', tempPath)
+}
+
+
+// functionality for upload images from array
+const uploader = async (id, img) => {
+    const formData = new FormData()
+    formData.append('image', img)
+
+    // await is for making the ajax req asynchronusly for uploading a bunch of image one by one
+    await $.ajax({
+        type: 'POST',
+        url: 'http://192.168.10.91:5000/upload-image', //'http://localhost:4500/upload-img',
+        data: formData,
+        contentType: false,
+        // enctype: 'multipart/form-data',
+        xhr: function () {
+            const xhr = new XMLHttpRequest()
+            xhr.upload.addEventListener('progress', (e) => {
+                const { loaded, total } = e;
+                const percentage = Math.ceil(loaded / total * 100)
+                const updatedHeight = (100 - percentage)
+                $(`#box-overlay-${id}`).css("height", `${updatedHeight}%`)
+                $(`#title-${id}`).text(`${percentage}%`)
+                // console.log(percentage)
+            }, false)
+            return xhr
+        },
+        processData: false,
+        success: (result, status, xhr) => {
+            console.log(result, status, xhr)
+        }
+    })
+}
+
+// functionality for uploading a single specific image 
+const uploadSingleImg = (id) => {
+    const input = $(document.createElement('input'))
+    input.attr('type', 'file')
+    console.log(input)
+    input.trigger('click')
+    input.on('change', async function (e) {
+        let data;
+        data = e.target.files[0]
+        $(`#outer-box-${id}`).empty()
+        const tempPath = URL.createObjectURL(data)
+        console.log(tempPath)
+        $(`#outer-box-${id}`).append(`
+            <img src="" alt="" id="img-${id}" class="img">
+            <div id="box-overlay-${id}" class="box-overlay"></div>
+            <p id="title-${id}" class="title"></p>
+        `)
+        $(`#img-${id}`).attr('src', tempPath)
+        
+        await uploader(id, data)
+    })
+
+}
+
+// making promise for img.onload
+const imgPromise = (img) => {
+    const tempPath = URL.createObjectURL(img)
+    let image = new Image()
+    image.src = tempPath
+    return new Promise((resolve, reject) => {
+        image.onload = function () {
+            resolve({
+                width: this.width,
+                height: this.height
+            })
+        };
+    })
+}
+
+// click event handler for retry button
+$(document).on('click', '.retry-btn', function () {
+    console.log(this)
+    const id = $(this).attr('id').split('-')[1]
+    uploadSingleImg(id)
+})
+
+
+// click event handler for the submit button for all images to upload
+$('#submit-btn').on('click', async function (e) {
     e.preventDefault()
     const data = $('.img-upload')[0].files
-    
-    for(const [id, img] of [...data].entries()){
-        console.log(id, img)
-        const formData = new FormData()
-        formData.append('img', img)
+    for (const [id, img] of [...data].entries()) {
+        imgDivMaker(id, img)
+    }
 
-        $.ajax({
-            type: 'POST',
-            url: 'http://localhost:4500/upload-img',
-            data: formData,
-            contentType: false,
-            xhr: function() {
-                const xhr = new XMLHttpRequest()
-                xhr.upload.addEventListener('progress', (e) => {
-                    const {loaded, total} = e;
-                    const percentage = Math.ceil(loaded / total * 100)
-                    $('.box-overlay').css("height", `${percentage}px`)
-                    $('.title').text(`${percentage}%`)
-                    console.log(percentage)
-                })
-                return xhr
-            },
-            enctype: 'multipart/form-data',
-            processData: false,
-            // success: (result, status, xhr) => {
-            //     console.log(result, status, xhr)
-            // }
-        })
+    for (const [id, img] of [...data].entries()) {
+        
+        const res = await imgPromise(img)
+        if (res.width > 1050 || res.height > 750) {
+            $(`#box-overlay-${id}`).css("background", `red`)
+            $(`#outer-box-${id}`).append(`<button class="retry-btn" id="retry-${id}">Retry</button>`)
+            $(`#box-overlay-${id}`).addClass('error')
+            continue;
+        }
+        await uploader(id, img)
     }
 })
+
 
 
